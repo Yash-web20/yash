afcee8d8c66e6af6b5b63c1fbf70bd3fd2fe8a98
yashp2098 <54570123+yashp2098@users.noreply.github.com>
2021-10-09 12:03:19 +0530

    update

--- temp.ps1
+++ temp.ps1
@@ -1,5 +1,3 @@
-new-eventlog -LogName "WVD" -Source "WVD"
-
 $msg = $env:UserName
 
 Write-EventLog -LogName "WVD" -Source "WVD" -EventID 3001 -EntryType Information -Message $msg -Category 1 -RawData 10,20
