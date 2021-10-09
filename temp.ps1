102948d91a535434d8170b7b71a66dfa44c12a86
yashp2098 <54570123+yashp2098@users.noreply.github.com>
2021-10-09 12:13:35 +0530

    update

--- temp.ps1
+++ temp.ps1
@@ -1,3 +1,15 @@
-$msg = $env:UserName
+try {
+    $connectTestResult = Test-NetConnection -ComputerName nistpocfilesharesa.file.core.windows.net -Port 445
+    if ($connectTestResult.TcpTestSucceeded) {
+        # Mount the drive
+        New-PSDrive -Name Z -PSProvider FileSystem -Root "\\nistpocfilesharesa.file.core.windows.net\profiles" -Persist
+    } else {
+        Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
+    }
 
-Write-EventLog -LogName "WVD" -Source "WVD" -EventID 3001 -EntryType Information -Message $msg -Category 1 -RawData 10,20
+    Write-EventLog -LogName "WVD" -Source "WVD" -EventID 3001 -EntryType Information -Message "Sucess" -Category 1 -RawData 10,20
+}
+catch {
+    Write-EventLog -LogName "WVD" -Source "WVD" -EventID 3001 -EntryType Information -Message "Fail" -Category 1 -RawData 10,20
+    
+}
