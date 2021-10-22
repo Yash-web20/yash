5432e0098195c276c4917f66d2d7744017c650b1
yashp2098 <54570123+yashp2098@users.noreply.github.com>
2021-10-22 16:11:14 +0530

    Update temp.ps1
    update

--- temp.ps1
+++ temp.ps1
@@ -1,11 +1,5 @@
-$securePassword = ConvertTo-SecureString "dRoFOzU!1D4@j&D4*#tR" -AsPlainText -force
-$credential = New-Object System.Management.Automation.PsCredential("mobiuslogic.com\itadmin",$securePassword)
-$session = New-PSSession -computername wvd-poc-vm-0 -credential $credential
-$command = {$connectTestResult = Test-NetConnection -ComputerName nistpocfilesharesa.file.core.windows.net -Port 445
-    if ($connectTestResult.TcpTestSucceeded) {
-        # Mount the drive
-        New-PSDrive -Name Z -PSProvider FileSystem -Root "\\nistpocfilesharesa.file.core.windows.net\profiles" -Persist
-    } else {
-        Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
-    }}
-Invoke-Command -session $session -scriptblock $command
+ # Install chocolatey
+ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    
+ # install FSLogix with chocolatey
+ choco install fslogix -y
