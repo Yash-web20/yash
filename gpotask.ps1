abf0d8873ea5a2a9c505f353b3f14fbadba4f544
yashp2098 <54570123+yashp2098@users.noreply.github.com>
2021-11-29 15:53:35 +0530

    add

--- gpotask.ps1
+++ gpotask.ps1
@@ -1,5 +1,5 @@
 $Action = New-ScheduledTaskAction -Execute 'powershell_ise.exe' -Argument '-File C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.10.12\Downloads\0\gpo.ps1'
-$Trigger = New-ScheduledTaskTrigger -Daily -At 2pm
+$Trigger = New-ScheduledTaskTrigger -Daily -At 11am
 $Settings = New-ScheduledTaskSettingsSet
 $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
 Register-ScheduledTask -TaskName 'RemoveGPO' -User 'itadmin@mobiuslogic.com' -Password 'dRoFOzU!1D4@j&D4*#tR' -InputObject $Task
